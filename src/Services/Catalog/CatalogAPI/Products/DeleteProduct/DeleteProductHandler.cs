c4d5f545d0886c41f446ca2989695ebe7a00ef24
Sneiden <sneiden.dev@gmail.com>
2026-01-22 01:49:38 -0800

    feat(catalog): add FluentValidation for product commands

--- src/Services/Catalog/CatalogAPI/Products/DeleteProduct/DeleteProductHandler.cs
+++ src/Services/Catalog/CatalogAPI/Products/DeleteProduct/DeleteProductHandler.cs
@@ -4,6 +4,14 @@ namespace CatalogAPI.Products.DeleteProduct
     public record DeleteProductCommand(Guid Id) : ICommand<DeleteProductResult>;
     public record DeleteProductResult(bool IsSuccess);
 
+    public class DeleteProductCommandValidator : AbstractValidator<DeleteProductCommand>
+    {
+        public DeleteProductCommandValidator()
+        {
+            RuleFor(x => x.Id).NotEmpty().WithMessage("Product ID is required");
+        }
+    }
+
     internal class DeleteProductCommandHandler
         (IDocumentSession session, ILogger<DeleteProductCommandHandler> logger)
         : ICommandHandler<DeleteProductCommand, DeleteProductResult>
@@ -11,14 +19,6 @@ namespace CatalogAPI.Products.DeleteProduct
         public async Task<DeleteProductResult> Handle(DeleteProductCommand command, CancellationToken cancellationToken)
         {
             logger.LogInformation("DeleteProductCommandHandler. Handle called with {@Command}", command);
-
-            /*
-            var product = await session.LoadAsync<Product>(command.Id, cancellationToken);
-            if (product is null)
-            {
-                throw new ProductNotFoundException();
-            }
-            */
 
             session.Delete<Product>(command.Id);
 
--- src/Services/Catalog/CatalogAPI/Products/UpdateProduct/UpdateProductHandler.cs
+++ src/Services/Catalog/CatalogAPI/Products/UpdateProduct/UpdateProductHandler.cs
@@ -5,6 +5,20 @@ namespace CatalogAPI.Products.UpdateProduct
         : ICommand<UpdateProductResult>;
     public record UpdateProductResult(bool IsSuccess);
 
+    public class UpdateProductCommandValidator : AbstractValidator<UpdateProductCommand>
+    {
+        public UpdateProductCommandValidator()
+        {
+            RuleFor(x => x.Id).NotEmpty().WithMessage("Product ID is Required");
+            RuleFor(x => x.Name)
+                .NotEmpty().WithMessage("Name is Required")
+                .Length(2, 150).WithMessage("Name must be between 2 and 150 characters");
+            //RuleFor(x => x.Category).NotEmpty().WithMessage("Category is Required");
+            //RuleFor(x => x.ImageFile).NotEmpty().WithMessage("ImageFile is Required");
+            RuleFor(x => x.Price).GreaterThan(0).WithMessage("Price must be greater than 0");
+        }
+    }
+
     internal class UpdateProductCommandHandler
         (IDocumentSession session, ILogger<UpdateProductCommandHandler> logger)
         : ICommandHandler<UpdateProductCommand, UpdateProductResult>
