2baa8133fad451717cfceae8ca59771b21f92973
Sneiden <sneiden.dev@gmail.com>
2026-02-02 07:14:32 -0800

    refactor(catalog): remove ILogger from handlers in favor of logging behavior

--- src/Services/Catalog/CatalogAPI/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/CatalogAPI/Products/CreateProduct/CreateProductHandler.cs
@@ -17,14 +17,12 @@ namespace CatalogAPI.Products.CreateProduct
     }
 
     internal class CreateProductCommandHandler
-        (IDocumentSession session, ILogger<CreateProductCommandHandler> logger)
+        (IDocumentSession session)
         : ICommandHandler<CreateProductCommand, CreateProductResult>
     {
         public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
         {
             // Business logic to create a product
-
-            logger.LogInformation("CreateProductCommandHandler. Handle called with {@Command}", command);
 
             // create product entity from command object
             var product = new Product
