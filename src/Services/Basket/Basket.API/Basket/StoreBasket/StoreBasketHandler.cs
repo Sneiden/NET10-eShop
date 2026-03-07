0f301dea6da98e2179fbbe35145a2459069ac0b9
Sneiden <45449601+Sneiden@users.noreply.github.com>
2026-03-06 18:54:25 -0800

    Implement Basket–Discount gRPC communication (#6)
    * feat(basket): add Discount gRPC client via connected services
    
    * feat(basket): register Discount gRPC client in dependency injection
    
    * infra(basket): configure Discount gRPC integration for docker environment
    
    - Add Discount gRPC dependency to basket.api in docker-compose
    - Configure GrpcSettings__DiscountUrl environment variable
    - Rebuild Basket.API container with new configuration
    - Bypass SSL certificate validation for gRPC client (development only)
    
    ---------
    
    Co-authored-by: Sneiden

--- src/Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketHandler.cs
+++ src/Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketHandler.cs
@@ -1,4 +1,7 @@
-﻿namespace Basket.API.Basket.StoreBasket
+﻿using Discount.Grpc;
+using JasperFx.Events.Daemon;
+
+namespace Basket.API.Basket.StoreBasket
 {
     public record StoreBasketCommand(ShoppingCart Cart) : ICommand<StoreBasketResult>;
     public record StoreBasketResult(string UserName);
@@ -10,16 +13,26 @@
             RuleFor(x => x.Cart.UserName).NotEmpty().WithMessage("UserName is required");
         }
     }
-    public class StoreBasketCommandHandler(IBasketRepository repository)
+    public class StoreBasketCommandHandler
+        (IBasketRepository repository, DiscountProtoService.DiscountProtoServiceClient discountProto)
         : ICommandHandler<StoreBasketCommand, StoreBasketResult>
     {
         public async Task<StoreBasketResult> Handle(StoreBasketCommand command, CancellationToken cancellationToken)
         {
-            ShoppingCart cart = command.Cart;
+            await DeductDiscount(command.Cart, cancellationToken);
 
             await repository.StoreBasket(command.Cart, cancellationToken);
 
             return new StoreBasketResult(command.Cart.UserName);
+        }
+        private async Task DeductDiscount(ShoppingCart cart, CancellationToken cancellationToken)
+        {
+            //TODO: comunicate with Discount.Grpc and calculate lastest price of products into shopping cart
+            foreach (var item in cart.Items)
+            {
+                var coupon = await discountProto.GetDiscountAsync(new GetDiscountRequest { ProductName = item.ProductName }, cancellationToken: cancellationToken);
+                item.Price -= coupon.Amount;
+            }
         }
     }
 }
--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -1,9 +1,12 @@
+using Discount.Grpc;
 using HealthChecks.UI.Client;
 using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container
+
+//Application Services
 var assembly = typeof(Program).Assembly;
 builder.Services.AddCarter();
 builder.Services.AddMediatR(config =>
@@ -12,6 +15,8 @@ builder.Services.AddMediatR(config =>
     config.AddOpenBehavior(typeof(ValidationBehavior<,>));
     config.AddOpenBehavior(typeof(LoggingBehavior<,>));
 });
+
+//Data Services
 builder.Services.AddMarten(opts =>
 {
     opts.Connection(builder.Configuration.GetConnectionString("Database")!);
@@ -19,12 +24,29 @@ builder.Services.AddMarten(opts =>
 }).UseLightweightSessions();
 builder.Services.AddScoped<IBasketRepository, BasketRepository>();
 builder.Services.Decorate<IBasketRepository, CachedBasketRepository>();// using scrutor
-builder.Services.AddExceptionHandler<CustomExceptionHandler>();
 builder.Services.AddStackExchangeRedisCache(options =>
 {
     options.Configuration = builder.Configuration.GetConnectionString("Redis");
     //option.Instance = "Basket";
 });
+
+//Grpc Services
+builder.Services.AddGrpcClient<DiscountProtoService.DiscountProtoServiceClient>(options =>
+{
+    options.Address = new Uri(builder.Configuration["GrpcSettings:DiscountUrl"]!);
+})
+.ConfigurePrimaryHttpMessageHandler(() =>
+{
+    var handler = new HttpClientHandler
+    {
+        ServerCertificateCustomValidationCallback =
+        HttpClientHandler.DangerousAcceptAnyServerCertificateValidator
+    };
+    return handler;
+});
+
+//Cross-Cutting Services
+builder.Services.AddExceptionHandler<CustomExceptionHandler>();
 builder.Services.AddHealthChecks()
     .AddNpgSql(builder.Configuration.GetConnectionString("Database")!)
     .AddRedis(builder.Configuration.GetConnectionString("Redis")!);
